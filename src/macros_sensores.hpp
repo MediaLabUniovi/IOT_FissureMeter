0a6d468083f4973a23a041c44169401b6d2c4216
Rubén Concejo Martínez <rubenconcejomartinez@gmail.com>
2022-06-20 13:09:58 +0200

    Añadir comentarios, quitar pines no usados y poner los pines SPI como macros

--- src/macros_sensores.hpp
+++ src/macros_sensores.hpp
@@ -1,16 +1,27 @@
 #include <Arduino.h>
 
+// Encoder
 #define pinCLK PB9
 #define pinDATA PB8
+
+// Humedad y lluvia (Analógios)
 #define pinRAIN PB0
 #define pinHUM PB1
+
+// Temperatura del suelo
 #define pinDS18B20 PB5
+
+// Datos del ambiente
 #define pinBME280_SDA PB7
 #define pinBME280_SCL PB6
+
+// Nivel de batería
 #define pinBAT PA2
 
-#define pinR PA10
-#define pinG PA11
-#define pinB PA12
+// RFM95
+#define pinMOSI PA7
+#define pinMISO PA6
+#define pinSCLK PA5
+
 
 
