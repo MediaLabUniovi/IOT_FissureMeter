95ba5d51f8e65766c94eeca4abb3ac572b50fff6
Miguel Enterria Lastra <miguelenterria@gmail.com>
2024-10-23 11:53:26 +0200

    Primer commit de Miguel sin cambios importantes

---
+++ src/configuration.h
@@ -0,0 +1,25 @@
+// configuration.h
+#pragma once
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+extern Adafruit_BME280 bme280;  // Declaración externa para que otros archivos lo usen
+
+#define pinDS18B20 PB5
+#define pinEncoderClk PB9
+#define pinEncoderData PB8
+#define pinHUM PB1
+#define pinBat PA2
+
+int batt;
+
+
+
+
